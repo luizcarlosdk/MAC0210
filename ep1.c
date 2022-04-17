282e90cd3a2794d0d43e39be733674a2e10c3324
Rodrigo Orem <orem.rodrigo@gmail.com>
2022-04-17 00:02:53 -0300

    Improve error handling and user input
    - Implement relative tolerance (rtol) stop criteria.
    - Ask user stop parameters max iterations and rtol.
    - Offer to user default values for every parameter asked.
    - Detect overflow and division by zero error.

--- ep1.c
+++ ep1.c
@@ -1,43 +1,145 @@
-#include <stdio.h>
 #include <complex.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-double g1(double x) {
-    double y = x + (exp(x) - 2* pow(x,2));
-    return y;
+#define MAX_ITER 100
+#define REL_TOL 0.001
+int maxIter = MAX_ITER;
+double relTol = REL_TOL;
+
+double g1(double x) { return x + (exp(x) - 2 * pow(x, 2)); }
+
+double g2(double x) { return -sqrt(exp(x) / 2); }
+
+double g3(double x) { return x - (exp(x) - 2 * pow(x, 2)) / (exp(x) - 4 * x); }
+
+bool hasConverged(double x1, double x2) {
+    return fabs(x1 - x2) < relTol * fabs(x1);
 }
 
-double g2(double x) {
-    double y = -sqrt(exp(x)/2);
-    return y;
+double fixedPoint(double x0, double (*f)(double)) {
+    double x = f(x0);
+    double prevX = x0;
+    bool endedGracefully = false;
+    for (int i = 0; i < maxIter; i++) {
+        prevX = x;
+        x = f(x);
+
+        if (isnan(x) || isinf(x)) {
+            break;
+        }
+
+        if (hasConverged(prevX, x)) {
+            endedGracefully = true;
+            break;
+        }
+    }
+
+    // TODO: como detectar o erro IterationsExceeded?
+
+    return x;
 }
 
-double g3(double x) {
-    float y = x - (exp(x) - 2*pow(x,2))/(exp(x) - 4*x);
-    return y;
+enum Status {
+    Pending,
+    Success,
+    IterationsExceeded,
+    Overflow,
+    DivisionByZero
+};
+
+struct RootStruct {
+    double x0;
+    double (*g)(double);
+    enum Status status;
+    double result;
+};
+
+void printResult(int rootIndex, double x, enum Status status) {
+    if (status == IterationsExceeded) {
+        printf("Erro: O cálculo da raiz %d excedeu o limite de iterações.\n",
+               rootIndex);
+    } else if (status == Overflow) {
+        printf("Erro: No cálculo da raiz %d houve overflow.\n", rootIndex);
+        return;
+    } else {
+        printf("A raiz %d convergiu para %f.\n", rootIndex, x);
+    }
+}
+
+void askStopParams() {
+    char input[128];
+    printf("CRITÉRIOS DE PARADA\n"
+           "===================\n");
+    printf("Qual o número de iterações máximo?\n"
+           "(deixe vazio usar o padrão %d)\n",
+           maxIter);
+    fgets(input, 128, stdin);
+    if (input[0] != '\n') {
+        maxIter = atoi(input);
+    }
+
+    printf("\nQual a constante rtol, para o erro relativo?\n"
+           "(deixe vazio para usar o padrão %f)\n",
+           relTol);
+    fgets(input, 128, stdin);
+    if (input[0] != '\n') {
+        relTol = strtod(input, NULL);
+    }
+
+    printf("\n\n");
+}
+
+void askPoints(struct RootStruct *roots, int rootsLength) {
+    printf("PONTOS INICIAIS\n"
+           "===============\n");
+
+    for (int i = 0; i < rootsLength; i++) {
+        printf("Qual o valor do ponto inicial da raiz %d?\n"
+               "(deixe vazio para usar o padrão %f)\n\n",
+               i, roots[i].x0);
+
+        char input[128];
+        fgets(input, 128, stdin);
+        if (input[0] != '\n') {
+            roots[i].x0 = strtod(input, NULL);
+        }
+    }
+
+    printf("\n");
+}
+
+void calculateRoots(struct RootStruct *roots, int rootsLength) {
+    for (int i = 0; i < rootsLength; i++) {
+        roots[i].result = fixedPoint(roots[i].x0, roots[i].g);
+        if (isinf(roots[i].result)) {
+            roots[i].status = Overflow;
+        } else if (isnan(roots[i].result)) {
+            roots[i].status = DivisionByZero;
+        } else {
+            roots[i].status = Success;
+        }
+    }
+}
+
+void printResults(struct RootStruct *roots, int rootsLength) {
+    printf("RESULTADOS\n"
+           "==========\n");
+
+    for (int i = 0; i < rootsLength; i++) {
+        printResult(i + 1, roots[i].result, roots[i].status);
+    }
 }
 
 int main() {
-    double x1,x2,x3;
-    double root0,root1,root2,i;
-    printf("Digite o valor de x0 para a primeira raiz: ");
-    scanf("%lf",&root0);
-    printf("Digite o valor de x0 para a segunda raiz: ");
-    scanf("%lf",&root1);
-    printf("Digite o valor de x0 para a terceira raiz: ");
-    scanf("%lf",&root2);
-    x1 = g1(root0);
-    x2 = g2(root1);
-    x3 = g3(root2);
+    askStopParams();
 
-    for (i=0; i != 100; i++) {
-        x1 = g1(x1);
-        x2 = g2(x2);
-        x3 = g3(x3);
-    }
+    struct RootStruct roots[3] = {
+        {.x0 = 2, .g = g1}, {.x0 = 3, .g = g2}, {.x0 = 3, .g = g3}};
 
-    printf("A primeira raiz é: %lf", x1);
-    printf("A segunda raiz é: %lf", x2);
-    printf("A terceira raiz é: %lf", x3);
-
+    askPoints(roots, 3);
+    calculateRoots(roots, 3);
+    printResults(roots, 3);
 }
