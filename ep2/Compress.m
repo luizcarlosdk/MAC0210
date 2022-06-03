f79e67888d2986bf6b4615cdf71b42545b16f07e
luizcarlosdk <luizcarlosdk@hotmail.com>
2022-06-03 16:39:21 -0300

    add funtional function
    Signed-off-by: luizcarlosdk <luizcarlosdk@hotmail.com>

--- ep2/Compress.m
+++ ep2/Compress.m
@@ -2,9 +2,8 @@
 function compress(originalImg, k)
   image = imread(originalImg);
   p = size(image)(1)
-  n = (p + k)/(1+k)
+  n = floor((p + k)/(1+k))
   p = n + (n-1)*k;
-  #image;
   solution = zeros(n,n,3);
   indices = [1 ];
   for i = 1:p
@@ -12,22 +11,17 @@ function compress(originalImg, k)
       indices = [indices i];
     endif
   endfor
-  ii = 1
-  jj = 1
+  ii = 1;
+  jj = 1;
   for i = indices
     for j = indices
       solution(ii,jj,:) = image(i,j,:);
-      jj = jj + 1
+      jj = jj + 1;
     endfor
-    jj = 1
-    ii = ii + 1
+    jj = 1;
+    ii = ii + 1;
   endfor
-
-  imwrite(solution, "/home/luizz/Documents/materias/MAC0210/ep2/anime2.jpg")
-
-  indices;
-
-  solution
+  imwrite (uint8(solution), "compressed.png", "Compression", "none");
 endfunction
 
-compress("/home/luizz/Documents/materias/MAC0210/ep2/anime.jpg",2)
+compress("/home/luizz/Documents/materias/MAC0210/ep2/anime.jpg",5)
